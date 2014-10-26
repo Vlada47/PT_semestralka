a49ff56afe6abcb92e3ac1d90ec0911266598d90
Vlada47 <laznickav@hotmail.cz>
2014-10-26 18:01:26 +0100

    change

--- src/semestralka/NakladniAuto.java
+++ src/semestralka/NakladniAuto.java
@@ -10,11 +10,7 @@ public class NakladniAuto {
 	private int ID;
 	private int pocetPlnychSudu;
 	private int pocetPrazdnychSudu;
-	private ArrayList<Integer> prazdnychSuduKNalozeni;
-	private ArrayList<Integer> plnychSuduKVylozeni;
 	private double vzdalenost;
-	private ArrayList<ArrayList<Integer>> cesty;
-	private int indexVykonavaneCesty = 0;
 	
 	public NakladniAuto(int ID) {
 		this.ID = ID;
@@ -24,30 +20,11 @@ public class NakladniAuto {
 		return this.ID;
 	}
 	
-	public void addCesta(int odkud, int kam, int prazdneSudy, int plneSudy) {
-		ArrayList<Integer> cesta = Matice.getNejkratsiCesta(odkud, kam);
-		this.cesty.add(cesta);
-		this.prazdnychSuduKNalozeni.add(prazdneSudy);
-		this.plnychSuduKVylozeni.add(plneSudy);
+	public void setVzdalenost(double vzdalenost) {
+		this.vzdalenost = vzdalenost;
 	}
 	
-	public void nalozPrazdneSudy(int pocet) {
-		this.vzdalenost += (pocet * StaticData.HODIN_NA_SUD) * this.RYCHLOST;
-		this.pocetPrazdnychSudu += pocet;
-	}
-	
-	public void vylozPrazdneSudy(int pocet) {
-		this.vzdalenost += (pocet * StaticData.HODIN_NA_SUD) * this.RYCHLOST;
-		this.pocetPrazdnychSudu -= pocet;
-	}
-	
-	public void nalozPlneSudy(int pocet) {
-		this.vzdalenost += (pocet * StaticData.HODIN_NA_SUD) * this.RYCHLOST;
-		this.pocetPlnychSudu += pocet;
-	}
-	
-	public void vylozPlneSudy(int pocet) {
-		this.vzdalenost += (pocet * StaticData.HODIN_NA_SUD) * this.RYCHLOST;
-		this.pocetPlnychSudu -= pocet;
+	public double getVzdalenost() {
+		return this.vzdalenost;
 	}
 }
